d0fb9f4707c154e4c84c120e0c7150c86097a3ff
quang20o2 <140184040+quang20o2@users.noreply.github.com>
2024-08-18 07:00:49 +0700

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-export const baseUrl = import.meta.env.APP_API_URL || 'http://localhost:4000'
+export const baseUrl = import.meta.env.APP_API_URL || 'https://foodweb-backend-qpua.onrender.com'
 
 export const api = axios.create({ baseURL: baseUrl })
